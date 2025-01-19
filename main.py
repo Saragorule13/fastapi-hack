1aae72258351cd1ad4aeef92c26221b949f75c40
Sara Gorule <136723426+Saragorule13@users.noreply.github.com>
2025-01-19 09:04:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Replace with your frontend URL
+    allow_origins=["https://level-supermind-hackathon-rouge.vercel.app"],  # Replace with your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
